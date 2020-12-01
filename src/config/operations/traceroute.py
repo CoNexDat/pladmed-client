985e9a50a0417b73f8f43a5d2c7c19337f5eec38
DamiCassinotti <damiancassinotti@gmail.com>
2020-12-01 20:28:31 -0300

    ping: added options

--- src/config/operations/traceroute.py
+++ src/config/operations/traceroute.py
@@ -21,15 +21,14 @@ TRACEROUTE_PARAMS = {
 }
 
 PING_PARAMS = {
-    '''"confidence": CommandManager("-c", BetweenValidator(0, 0.99)),
-    "method": CommandManager("-P", MultiValueValidator(["udp-paris", "icmp-paris", "icp"])),
+    "probecount": CommandManager("-c", BetweenValidator(1, 100)),
+    "icmp-sum": CommandManager("-C", AnyValidator()),
     "dport": CommandManager("-d", AnyValidator()),
-    "firsthop": CommandManager("-f", AnyValidator()),
-    "maxttl": CommandManager("-m", BetweenValidator(1, 255)),
-    "attempts": CommandManager("-q", BetweenValidator(1, 10)),
-    "sport": CommandManager("-s", AnyValidator()),
-    "wait": CommandManager("-w", BetweenValidator(1, 20)),
-    "wait-probe": CommandManager("-W", BetweenValidator(0, 100))'''
+    "sport": CommandManager("-F", AnyValidator()),
+    "wait": CommandManager("-i", BetweenValidator(1, 20)),
+    "method": CommandManager("-P", MultiValueValidator(["icmp-echo", "icmp-time", "tcp-syn", "tcp-ack", "tcp-ack-sport", "udp", "udp-dport"])),
+    "size": CommandManager("-s", BetweenValidator(1, 255)),
+    "timeout": CommandManager("-W", BetweenValidator(0, 100))
 }
 
 GENERAL_PARAMS = {
