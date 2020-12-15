2706ee9388f8ccadccff22703419ce1380207a83
fedefunes96 <fede.funes96@gmail.com>
2020-12-15 19:59:20 -0300

    Elimino codigo comentado obsoleto

--- src/common/client.py
+++ src/common/client.py
@@ -13,15 +13,6 @@ class Client:
     def connect(self):
         print("Client connected")
 
-        '''try:
-            response = urllib.request.urlopen('http://0.0.0.0:5000/')
-            print(json.loads(response.read()))
-        except:
-            print("Can't reach server")
-
-        subprocess.run(["dig", "www.google.com"])'''
-        #result = subprocess.run(["scamper", "-c", "trace -P UDP-paris", "-i", "179.60.195.36"])
-
     def traceroute(self, params):
         # Params must be a dict with params
         sub_cmd = self.parser.parse_traceroute(params)
