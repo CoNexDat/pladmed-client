96b6d0513a70f09b5927dbde2323096680ee9fc2
DamiCassinotti <damiancassinotti@gmail.com>
2021-02-20 00:18:22 -0300

    Sched: Manejo de parametros desde el backend

--- src/common/client.py
+++ src/common/client.py
@@ -18,28 +18,28 @@ class Client:
     def traceroute(self, op_id, params):
         # Params must be a dict with params
         sub_cmd = self.parser.parse_traceroute(params)
-        self.execute_scamper(op_id, sub_cmd)
+        self.execute_scamper(op_id, sub_cmd, params["cron"], params["times_per_minute"], params["stop_time"])
 
     def ping(self, op_id, params):
         # Params must be a dict with params
         sub_cmd = self.parser.parse_ping(params)
-        self.execute_scamper(op_id, sub_cmd)
+        self.execute_scamper(op_id, sub_cmd, params["cron"], params["times_per_minute"], params["stop_time"])
 
-    def execute_scamper(self, op_id, sub_cmd):
+    def execute_scamper(self, op_id, sub_cmd, cron_expression, times_per_minute, stop_time):
         print("Executing scamper -c with params: ", sub_cmd)
         # filename = self.storage.create_operation_filename(op_id)
         sub_cmd_str = " ".join([f"'{param}'" for param in sub_cmd])
-        cron_command = f"python3 /src/scripts/scamper.py {op_id} {sub_cmd_str}"
+        cron_command = f"python3 /src/scripts/scamper.py {op_id} {times_per_minute} {sub_cmd_str}"
         print(cron_command)
         # Saves execution cron
         with CronTab(user=True) as cron:
             job = cron.new(command=cron_command, comment=op_id)
-            job.setall('* * * * *')
+            job.setall(cron_expression)
         # Saves stopping cron
         with CronTab(user=True) as cron:
             stop_command = f"python3 /src/scripts/stopper.py {op_id}"
             job = cron.new(command=stop_command, comment=op_id)
-            job.setall('*/15 * * * *')
+            job.setall(stop_time)
 
     def disconnect(self):
         print("Client disconnected")
--- src/scripts/scamper.py
+++ src/scripts/scamper.py
@@ -1,17 +1,18 @@
 import subprocess
 import sys
+import time
 from os import path
 # from common.storage import Storage
 # import config.connection as config
 
 
 def create_operation_filename(op_id):
-    store_in = "results/"
+    store_in = "/src/results/"
     file_storage = store_in + op_id
 
     i = 1
     while path.exists(file_storage):
-        file_storage = op_id + "_part_" + str(i)
+        file_storage = store_in + op_id + "_part_" + str(i)
         i += 1
 
     return file_storage
@@ -20,18 +21,22 @@ def create_operation_filename(op_id):
 def main():
     print(sys.argv)
     op_id = sys.argv[1]
-    sub_cmd = sys.argv[2:]
-    filename = create_operation_filename(op_id)
-    print(subprocess.run(
-        [
-            "scamper",
-            "-O",
-            "warts",
-            "-o",
-            filename,
-            "-c"
-        ] + sub_cmd
-    ))
+    times_per_minute = int(sys.argv[2])
+    sub_cmd = sys.argv[3:]
+    for i in range(times_per_minute):
+        start = time.time()
+        filename = create_operation_filename(op_id)
+        subprocess.run(
+            [
+                "scamper",
+                "-O",
+                "warts",
+                "-o",
+                filename,
+                "-c"
+            ] + sub_cmd
+        )
+        time.sleep(max(60/times_per_minute - int(time.time() - start), 0))
 
 
 if __name__ == "__main__":
