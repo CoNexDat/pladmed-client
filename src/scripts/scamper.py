38d12658903fad1cbef09bfae4d071d1cd3a05dd
fedefunes96 <fede.funes96@gmail.com>
2021-03-02 07:08:50 -0300

    Elimino prints() de las funciones del scheduler

--- src/scripts/scamper.py
+++ src/scripts/scamper.py
@@ -23,8 +23,6 @@ def operation_filename(task):
     return file_storage 
 
 def end_task(operation_str, task, client):
-    print(f'Sending to FinishTaskCommunicator operation {operation_str}')
-
     finished_task_data = {
         "operation": json.loads(operation_str),
         "task": task.data()
@@ -33,26 +31,15 @@ def end_task(operation_str, task, client):
     client.send(json.dumps(finished_task_data))
 
 def main():
-    print(sys.argv)
-    print("Scamper main called")
-
     client = Client(address)
 
     times_per_minute = int(sys.argv[1])
 
-    print(f"times_per_minute: {times_per_minute}")
-
     sub_cmd_joined = sys.argv[2]
     
-    print(f"sub_cmd_joined: {sub_cmd_joined}")
-
     sub_cmd = sub_cmd_joined.split("|")
 
-    print("Sub cmd splitted: ", sub_cmd)
-
     operation_str = sys.argv[3]
-
-    print(f"operation_str: {operation_str}")
 
     for i in range(times_per_minute):
         start = time.time()
@@ -75,5 +62,4 @@ def main():
     client.close()
 
 if __name__ == "__main__":
-    print("Scamper called")
     main()
--- src/scripts/stopper.py
+++ src/scripts/stopper.py
@@ -10,33 +10,23 @@ from multiprocessing.connection import Client
 address = ('localhost', int(getenv('FINISH_OPERATION_PORT')))
 
 def end_operation(operation_str, client):
-    print(f'Sending to FinishOperationCommunicator operation {operation_str}')
     client.send(operation_str)
 
 def main():
-    print(sys.argv)
     client = Client(address)
 
     op_id = sys.argv[1]
 
-    print(f"op_id: {op_id}")
-
     operation_str = sys.argv[2]
-
-    print(f"operation_str: {operation_str}")
-
-    print("Removing a CRON")
 
     with CronTab(user=True) as cron:
         job_iter = cron.find_comment(op_id)
 
         for job in job_iter:
-            print("Removing job...")
             cron.remove(job)
 
     end_operation(operation_str, client)
     client.close()
 
 if __name__ == "__main__":
-    print("Stopper called")
     main()
