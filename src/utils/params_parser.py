276ec7070e229868f70701630ae6491fd26bfef1
dario-ramos <dario.fenix@gmail.com>
2021-03-09 15:59:26 -0300

    Resolve domain names to ips

--- src/utils/params_parser.py
+++ src/utils/params_parser.py
@@ -1,3 +1,6 @@
+import subprocess
+import ipaddress
+
 from config.operations.params import TRACEROUTE_PARAMS, GENERAL_PARAMS, PING_PARAMS, DNS_PARAMS
 
 
@@ -16,12 +19,14 @@ class ParamsParser:
         return command.rstrip()
 
     def parse_traceroute(self, params):
+        params = self.resolve_domain_names(params)
         general_cmd = self.parse_params(params, GENERAL_PARAMS).split(' ')
         sub_cmd = ["trace " + self.parse_params(params, TRACEROUTE_PARAMS)]
 
         return sub_cmd + general_cmd
 
     def parse_ping(self, params):
+        params = self.resolve_domain_names(params)
         general_cmd = self.parse_params(params, GENERAL_PARAMS).split(' ')
         sub_cmd = ["ping " + self.parse_params(params, PING_PARAMS)]
 
@@ -30,3 +35,24 @@ class ParamsParser:
     def parse_dns(self, params):
         general_cmd = self.parse_params(params, DNS_PARAMS).split(' ')
         return general_cmd
+
+    def resolve_domain_names(self, params):
+        if "domains" not in params:
+            return params
+        dig_cmd = ["dig", "+short"] + params["domains"]
+        dig_proc = subprocess.run(dig_cmd, stdout=subprocess.PIPE)
+        dig_output = dig_proc.stdout.decode("utf-8")
+        output_lines = dig_output.split('\n')
+        ips = []
+        for line in output_lines:
+            try:
+                ipaddress.ip_address(line)
+                ips.append(line.strip())
+            except ValueError:
+                continue
+        if "ips" not in params:
+            params["ips"] = ips
+        else:
+            params["ips"] = params["ips"] + ips
+        del params["domains"]
+        return params
